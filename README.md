4a559d00657a6c6fb8c3b79f59171f250b05d99a
katlyn-thomas <katlyn.c.thomas@gmail.com>
2023-09-10 23:27:01 -0500

    init, c2z babeeee

---
+++ README.md
@@ -0,0 +1,9 @@
+# Clip2Zero
+
+## Building
+
+After installing [Rust](https://rustup.rs/), you can compile Clip2Zero as follows:
+
+```shell
+cargo xtask bundle clip2zero --release
+```
---
+++ src/lib.rs
@@ -0,0 +1,179 @@
+use nih_plug::prelude::*;
+use std::sync::Arc;
+
+// This is a shortened version of the gain example with most comments removed, check out
+// https://github.com/robbert-vdh/nih-plug/blob/master/plugins/examples/gain/src/lib.rs to get
+// started
+
+struct Clip2Zero {
+    params: Arc<Clip2ZeroParams>,
+}
+
+#[derive(Params)]
+struct Clip2ZeroParams {
+    /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
+    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
+    /// parameters are exposed to the host in the same order they were defined. In this case, this
+    /// gain parameter is stored as linear gain while the values are displayed in decibels.
+    #[id = "input"]
+    pub input: FloatParam,
+
+    #[id = "compensate"]
+    pub compensate: EnumParam<Compensate>,
+}
+
+#[derive(Enum, PartialEq, Eq)]
+enum Compensate {
+    Off,
+    Half,
+    Equal,
+}
+
+impl Default for Clip2Zero {
+    fn default() -> Self {
+        Self {
+            params: Arc::new(Clip2ZeroParams::default()),
+        }
+    }
+}
+
+impl Default for Clip2ZeroParams {
+    fn default() -> Self {
+        Self {
+            // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
+            // to treat these kinds of parameters as if we were dealing with decibels. Storing this
+            // as decibels is easier to work with, but requires a conversion for every sample.
+            input: FloatParam::new(
+                "Input",
+                util::db_to_gain(0.0),
+                FloatRange::Skewed {
+                    min: util::db_to_gain(-30.0),
+                    max: util::db_to_gain(30.0),
+                    // This makes the range appear as if it was linear when displaying the values as
+                    // decibels
+                    factor: FloatRange::gain_skew_factor(-30.0, 30.0),
+                },
+            )
+            // Because the gain parameter is stored as linear gain instead of storing the value as
+            // decibels, we need logarithmic smoothing
+            .with_smoother(SmoothingStyle::Logarithmic(50.0))
+            .with_unit(" dB")
+            // There are many predefined formatters we can use here. If the gain was stored as
+            // decibels instead of as a linear gain value, we could have also used the
+            // `.with_step_size(0.1)` function to get internal rounding.
+            .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
+            .with_string_to_value(formatters::s2v_f32_gain_to_db()),
+
+            compensate: EnumParam::new("Gain Comp", Compensate::Off),
+        }
+    }
+}
+
+impl Plugin for Clip2Zero {
+    const NAME: &'static str = "clip2zero";
+    const VENDOR: &'static str = "Katlyn Thomas";
+    const URL: &'static str = env!("CARGO_PKG_HOMEPAGE");
+    const EMAIL: &'static str = "katlyn.c.thomas@gmail.com";
+
+    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+    // The first audio IO layout is used as the default. The other layouts may be selected either
+    // explicitly or automatically by the host or the user depending on the plugin API/backend.
+    const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[AudioIOLayout {
+        main_input_channels: NonZeroU32::new(2),
+        main_output_channels: NonZeroU32::new(2),
+
+        aux_input_ports: &[],
+        aux_output_ports: &[],
+
+        // Individual ports and the layout as a whole can be named here. By default these names
+        // are generated as needed. This layout will be called 'Stereo', while a layout with
+        // only one input and output channel would be called 'Mono'.
+        names: PortNames::const_default(),
+    }];
+
+
+    const MIDI_INPUT: MidiConfig = MidiConfig::None;
+    const MIDI_OUTPUT: MidiConfig = MidiConfig::None;
+
+    const SAMPLE_ACCURATE_AUTOMATION: bool = true;
+
+    // If the plugin can send or receive SysEx messages, it can define a type to wrap around those
+    // messages here. The type implements the `SysExMessage` trait, which allows conversion to and
+    // from plain byte buffers.
+    type SysExMessage = ();
+    // More advanced plugins can use this to run expensive background tasks. See the field's
+    // documentation for more information. `()` means that the plugin does not have any background
+    // tasks.
+    type BackgroundTask = ();
+
+    fn params(&self) -> Arc<dyn Params> {
+        self.params.clone()
+    }
+
+    fn initialize(
+        &mut self,
+        _audio_io_layout: &AudioIOLayout,
+        _buffer_config: &BufferConfig,
+        _context: &mut impl InitContext<Self>,
+    ) -> bool {
+        // Resize buffers and perform other potentially expensive initialization operations here.
+        // The `reset()` function is always called right after this function. You can remove this
+        // function if you do not need it.
+        true
+    }
+
+    fn reset(&mut self) {
+        // Reset buffers and envelopes here. This can be called from the audio thread and may not
+        // allocate. You can remove this function if you do not need it.
+    }
+
+    fn process(
+        &mut self,
+        buffer: &mut Buffer,
+        _aux: &mut AuxiliaryBuffers,
+        _context: &mut impl ProcessContext<Self>,
+    ) -> ProcessStatus {
+        for channel_samples in buffer.iter_samples() {
+            let input = self.params.input.smoothed.next();
+            let comp = self.params.compensate.value();
+
+            for sample in channel_samples {
+                *sample *= input;
+
+                if sample.abs() > 1. {
+                    *sample /= sample.abs();
+                }
+
+                match comp {
+                    Compensate::Off => continue,
+                    Compensate::Equal => *sample *= input.recip(),
+                    Compensate::Half => *sample *= (input.recip() + 1.) / 2.,
+                }
+            }
+        }
+
+        ProcessStatus::Normal
+    }
+}
+
+impl ClapPlugin for Clip2Zero {
+    const CLAP_ID: &'static str = "com.your-domain.clip2zero";
+    const CLAP_DESCRIPTION: Option<&'static str> = Some("A short description of your plugin");
+    const CLAP_MANUAL_URL: Option<&'static str> = Some(Self::URL);
+    const CLAP_SUPPORT_URL: Option<&'static str> = None;
+
+    // Don't forget to change these features
+    const CLAP_FEATURES: &'static [ClapFeature] = &[ClapFeature::AudioEffect, ClapFeature::Utility];
+}
+
+impl Vst3Plugin for Clip2Zero {
+    const VST3_CLASS_ID: [u8; 16] = *b"Exactly16Chars!!";
+
+    // And also don't forget to change these categories
+    const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] =
+        &[Vst3SubCategory::Fx, Vst3SubCategory::Dynamics, Vst3SubCategory::Tools];
+}
+
+nih_export_clap!(Clip2Zero);
+nih_export_vst3!(Clip2Zero);
